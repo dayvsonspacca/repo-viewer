08ee70f92eb193d9a9e844bed73f743d2c7ef070
Dayvson Spacca <spacca.dayvson@gmail.com>
2023-07-21 23:24:40 -0300

    Inicio do projeto, primeiro prototipo da tela.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,135 @@
-import Image from 'next/image'
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { TableCell, TableRow } from "@/components/ui/table";
+import { MagnifyingGlassIcon, PlusIcon } from "@radix-ui/react-icons";
+import { BiFileBlank, BiSolidFolder, BiSolidPencil, BiSolidTrashAlt } from "react-icons/bi";
 
 export default function Home() {
+  const files = [
+    {
+      fileName: "src",
+      fileType: "folder",
+    },
+    {
+      fileName: "public",
+      fileType: "folder",
+    },
+    {
+      fileName: ".eslintrc.json",
+      fileType: "file",
+    },
+    {
+      fileName: ".gitignore",
+      fileType: "file",
+    },
+    {
+      fileName: "LICENSE",
+      fileType: "file",
+    },
+    {
+      fileName: "README.md",
+      fileType: "file",
+    },
+    {
+      fileName: "next.config.js",
+      fileType: "file",
+    },
+    {
+      fileName: "package-lock.json",
+      fileType: "file",
+    },
+    {
+      fileName: "package.json",
+      fileType: "file",
+    },
+    {
+      fileName: "postcss.config.js",
+      fileType: "file",
+    },
+    {
+      fileName: "tailwind.config.js",
+      fileType: "file",
+    },
+    {
+      fileName: "tsconfig.json",
+      fileType: "file",
+    },
+  ];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="bg-slate-50 w-screen h-screen flex items-center justify-center flex-col">
+      <div className="w-4/5 flex justify-between m-5">
+        <div className="flex max-w-sm items-center gap-3">
+          <Input type="text" className="h-9" placeholder="Buscar arquivo.." />
+          <Button size="sm">
+            <MagnifyingGlassIcon className="h-4 w-4" />
+          </Button>
+        </div>
+
+        <div className="flex items-center gap-3 flex-row-reverse">
+          <Button size="sm">
+            <PlusIcon className="h-4 w-4 mr-3" /> Adicionar
+          </Button>
+          <Select>
+            <SelectTrigger className="w-[180px] h-9 bg-white">
+              <SelectValue placeholder="Tipo de arquivo..." />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="file">Arquivo</SelectItem>
+              <SelectItem value="folder">Pasta</SelectItem>
+            </SelectContent>
+          </Select>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <Card className="w-4/5">
+        <CardHeader className="h-9 bg-secondary flex items-center flex-row gap-2">
+          <Avatar className="w-8 h-8">
+            <AvatarImage
+              src="https://github.com/dayvsonspacca.png"
+              alt="@dayvsonspacca"
+            />
+            <AvatarFallback>DS</AvatarFallback>
+          </Avatar>
+        </CardHeader>
+        <CardContent className="p-0">
+          <ScrollArea className="h-96">
+            {files.map((file, key) => {
+              return (
+                <TableRow key={key}>
+                  <TableCell className="flex gap-3 items-center">
+                    {file.fileType === "folder" ? (
+                      <BiSolidFolder size={20} className="text-slate-600" />
+                    ) : (
+                      <BiFileBlank size={20} className="text-slate-600" />
+                    )}
+                    {file.fileName}
+                  </TableCell>
+                  <TableCell className="text-right space-x-2 p-2">
+                    <Button variant="ghost" size="icon" className="h-8 w-8">
+                      <BiSolidTrashAlt size={16} className="text-slate-600" />
+                    </Button>
+                    <Button variant="ghost" size="icon" className="h-8 w-8">
+                      <BiSolidPencil size={16} className="text-slate-600" />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </ScrollArea>
+        </CardContent>
+      </Card>
     </main>
-  )
+  );
 }
